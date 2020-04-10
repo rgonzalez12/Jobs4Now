f9e17d87bb372e304fb1d0d52a0580ebf5a51eb2
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-04-09 22:05:42 -0600

    added migrations, adjusted models and associations

---
+++ db/migrate/20200410031811_create_applications.rb
@@ -0,0 +1,6 @@
+class CreateApplications < ActiveRecord::Migration[6.0]
+  def change
+    create_table :applications do |t|
+    end
+  end
+end
---
+++ db/migrate/20200410032045_create_reviews.rb
@@ -0,0 +1,6 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+    end
+  end
+end
