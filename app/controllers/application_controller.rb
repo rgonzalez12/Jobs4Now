d339c6792611396778341be7eaf44f4306d43e7a
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-04-11 15:43:31 -0600

    added logic for application, sessions and users controller. also added additional skeleton view templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :verified
+    helper_method :current_user
+
+    def home
+    end
+
+    private
+
+    def verified
+      redirect_to '/' unless authenticated
+    end
+  
+    def authenticated
+      !!current_user
+    end
+  
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,42 @@
 class UsersController < ApplicationController
+    skip_before_action :verified, only: [:new, :create]
     
     def new
-
+      @user = User.new
     end
 
     def create
-
-    end
-
-    def update
-
+      if (user = User.create(user_params))
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        render 'new'
+      end
     end
 
     def edit
+      @user = User.find(params[:id])
+    end
 
+    def update
+      @user = User.find(params[:id])
+      @user.update(user_params)
+    
+      if @user.save
+        redirect_to @user
+      else
+        render :edit
+      end
     end
 
     def show 
-
+      @user = User.find_by(id: params[:id])
     end
 
-    def destroy
+    private
 
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :phone_number, :address, :bio)
     end
 
 end
