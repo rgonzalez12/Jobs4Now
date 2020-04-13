a10e10c405846785547f6ce048dd550de960b001
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-04-12 20:17:09 -0600

    added more controller logic, added routes and view logic for sign-up/login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,18 @@ class UsersController < ApplicationController
     end
 
     def create
-      if (user = User.create(user_params))
-        session[:user_id] = user.id
-        redirect_to user_path(user)
+      @user = User.new(user_params)
+
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to '/home'
       else
-        render 'new'
+        render :new
       end
+    end
+
+    def index
+
     end
 
     def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static#home'
+  resources :users, only: [:new, :create], path_names: {new: 'sign_up'}
 end
