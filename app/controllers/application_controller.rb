281c182c1ecbd457b7325559d937b81b8c55366a
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-06-03 22:03:01 -0600

    removed unused methods from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :verified
     helper_method :current_user, :logged_in?
 
     def home
     end
 
     private
-
-    def verified
-      #redirect_to '/' unless logged_in?
-    end
   
     def logged_in?
       !!current_user
