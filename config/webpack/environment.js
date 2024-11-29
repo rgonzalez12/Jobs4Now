635a9210f5d17d34867642809e68ee0c5badcbe1
Ray <raymondalexander.gonzalez@gmail.com>
2024-11-29 02:37:14 -0500

    Recreated gemfile.lock, recreated yarn.lock. Updated node version to 16, set compile for production, updated loader sequence for js environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+// Add Babel loader for ES6+ syntax
+const babelLoader = environment.loaders.get('babel');
+babelLoader.test = /\.(js|jsx|mjs)$/;
+babelLoader.exclude = /node_modules/;
+
+// Optional: Configure additional settings for performance or compatibility
+module.exports = environment;
