f19feb4323d11e25f3f8322ba156eaafdd1ed7d1
Ray <raymondalexander.gonzalez@gmail.com>
2024-11-28 21:18:58 -0500

    Disabled out omniauth for FB login method. Ran DB migrations, added webpacker gem.

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_002159) do
-
+ActiveRecord::Schema[8.0].define(version: 2020_06_02_002159) do
   create_table "event_registrations", force: :cascade do |t|
     t.integer "event_id"
     t.integer "attendee_id"
@@ -19,13 +18,13 @@ ActiveRecord::Schema.define(version: 2020_06_02_002159) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.datetime "event_time"
+    t.datetime "event_time", precision: nil
     t.string "name"
     t.string "description"
     t.string "agenda"
     t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "location"
     t.integer "attendee_id"
     t.string "host_name"
@@ -46,8 +45,8 @@ ActiveRecord::Schema.define(version: 2020_06_02_002159) do
     t.string "schedule"
     t.string "field_of_work"
     t.string "contact_info"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name"
     t.string "location"
     t.integer "applicant_id"
@@ -61,12 +60,11 @@ ActiveRecord::Schema.define(version: 2020_06_02_002159) do
     t.string "phone_number"
     t.string "address"
     t.string "bio"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "resume"
     t.text "cover_letter"
     t.string "uid"
     t.string "provider"
   end
-
 end
